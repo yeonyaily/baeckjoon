c1cf4be72f34195083cb21f22847234e6aac1f03
yeonyaily <yeonye330@gmail.com>
2023-01-10 18:37:37 +0900

    [Complete] 2438

---
+++ input_output/2438.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main(){
+    int N;
+    cin >> N;
+
+    for(int i=1; i<N+1; i++){
+        for(int j=0; j<i; j++){
+            cout << "*";
+        } 
+        cout << endl;
+    }
+
+    return 0;
+}
