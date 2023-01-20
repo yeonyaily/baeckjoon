2928eafddf7dce35322d53255167d9fb8f30a5b2
yeonyaily <yeonye330@gmail.com>
2023-01-20 23:28:58 +0900

    [Complete] 1934

---
+++ basic_math/1934.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+void num_split(int num, vector<int> &v);
+
+int main(){
+	int T;
+
+    cin >> T;
+
+    for(int i=0; i<T; i++){
+        int input1, input2, A=1, B;
+        vector<int> v1, v2;
+        cin >> input1 >> input2;
+
+	    num_split(input1, v1);
+	    num_split(input2, v2);
+
+	    while(v1.size()!=0){
+		    auto it = find(v2.begin(), v2.end(), v1.front());
+		    if(it != v2.end()){
+			    v2.erase(v2.begin() + (it-v2.begin()));
+			    A = A * v1.front();
+		    }
+		    v1.erase(v1.begin());
+	    }
+	    B = A * (input1/A) * (input2/A);
+	    cout << B << endl;
+    }
+
+	return 0;
+}
+
+void num_split(int num, vector<int> &v){
+	int div = 2;
+
+	if(num==1){
+		sort(v.begin(), v.end());
+		return;
+	}else{
+		while(num%div!=0) div++;
+		v.push_back(div);
+		num_split(num/div, v);
+	}
+}
