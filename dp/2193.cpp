29e11912130f17fd7852e125130934f1a5b99baf
yeonyaily <yeonye330@gmail.com>
2023-02-01 18:40:38 +0900

    [Complete] 2193

--- dp/2193.cpp
+++ dp/2193.cpp
@@ -3,11 +3,23 @@
 using namespace std;
 
 int main(){
-    int N;
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+
+    long long N, dp[91];
 
     cin >> N;
 
+    dp[0] = 0;
+    dp[1] = 1;
+    dp[2] = 1;
     
+    for(int i=3; i<N+1; i++){
+        dp[i] = dp[i-1] + dp[i-2];
+    }
+
+    cout << dp[N] << endl;
 
     return 0;
 }
